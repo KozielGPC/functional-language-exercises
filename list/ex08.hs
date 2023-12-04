914d50957f1f1daf34533b66374a0a8f4695ddab
KozielGPC <gpcgabriel0@gmail.com>
2023-12-04 14:54:39 -0300

    update: ex 8

--- list/ex08.hs
+++ list/ex08.hs
@@ -4,22 +4,24 @@ import System.Random
 criarListaAleatoria :: Int -> [Int]
 criarListaAleatoria n = take n $ randomRs (1, 100) $ mkStdGen 42
 
--- Função que calcula a interseção de duas listas
+-- Função que verifica a interseção de duas listas
 intersecaoListas :: Eq a => [a] -> [a] -> [a]
-intersecaoListas lista1 lista2 = intersect lista1 lista2
+intersecaoListas [] _ = []
+intersecaoListas (x:xs) ys
+  | x `elem` ys = x : intersecaoListas xs ys
+  | otherwise   = intersecaoListas xs ys
 
 main :: IO ()
 main = do
-    putStrLn "Tamanho da lista 1: "
-    tamanhoLista1 <- readLn
-    let lista1 = criarListaAleatoria tamanhoLista1
-    putStrLn $ "Lista 1: " ++ show lista1
+  putStrLn "Tamanho da lista 1: "
+  tamanhoLista1 <- readLn
+  let lista1 = criarListaAleatoria tamanhoLista1
+  putStrLn $ "Lista 1: " ++ show lista1
 
-    putStrLn "Tamanho da lista 2: "
-    tamanhoLista2 <- readLn
-    let lista2 = criarListaAleatoria tamanhoLista2
-    putStrLn $ "Lista 2: " ++ show lista2
+  putStrLn "Tamanho da lista 2: "
+  tamanhoLista2 <- readLn
+  let lista2 = criarListaAleatoria tamanhoLista2
+  putStrLn $ "Lista 2: " ++ show lista2
 
-    let intersecao = intersecaoListas lista1 lista2
-    putStrLn $ "Interseção das listas 1 e 2: " ++ show intersecao
-    
+  let intersecao = intersecaoListas lista1 lista2
+  putStrLn $ "Interseção das listas 1 e 2: " ++ show intersecao
