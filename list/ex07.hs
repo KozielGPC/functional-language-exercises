d532be938c7f8b0b272ad74bd47e1a7c6b392bd9
Eduardo Riki <eduardomatushita1@gmail.com>
2023-12-04 15:48:06 -0300

    feat: Edit ex07

--- list/ex07.hs
+++ list/ex07.hs
@@ -1,24 +1,11 @@
-import System.Random
-
--- Função que cria uma lista de tamanho n
-criarLista :: Int -> IO [Int]
-criarLista n = do
-    gen <- newStdGen
-    return $ take n $ randomRs (1, 100) gen
-
 concatenarListas :: [a] -> [a] -> [a]
 concatenarListas [] ys = ys
 concatenarListas (x:xs) ys = x : concatenarListas xs ys
 
 main :: IO ()
 main = do
-    putStrLn "tamanho da lista 1: "
-    tamanhoLista1 <- readLn
-    lista1 <- criarLista tamanhoLista1
+    let list1 = [3,4,2,7,1,8,2,1]
+    let list2 = [10,9,6,5,6,9,10]
 
-    putStrLn "tamanho da lista 2: "
-    tamanhoLista2 <- readLn
-    lista2 <- criarLista tamanhoLista2
-
-    let resultadoConcatenacao = concatenarListas lista1 lista2
-    putStrLn $ "A concatenação de " ++ show lista1 ++ " e " ++ show lista2 ++ " é: " ++ show resultadoConcatenacao
+    let resultadoConcatenacao = concatenarListas list1 list2
+    putStrLn $ "A concatenação de " ++ show list1 ++ " e " ++ show list2 ++ " é: " ++ show resultadoConcatenacao
